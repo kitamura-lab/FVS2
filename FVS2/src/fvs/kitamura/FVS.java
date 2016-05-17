0595cfc0b8c0fb531309284f16c844f58164572a
Yasuhiko Kitamura <ykitamura@kwansei.ac.jp>
2016-05-17 21:37:57 +0900

    バージョン番号の更新

--- FVS2/src/fvs/kitamura/FVS.java
+++ FVS2/src/fvs/kitamura/FVS.java
@@ -31,7 +31,7 @@ public class FVS {
 	private JButton[][] item = new JButton[COLMAX][ROWMAX];
 	private JPanel panel;
 
-	final String version = "2.2a";
+	final String version = "2.3";
 	final String LOGFILE = "FVS.log";
 	private Logger logger = null;
 
